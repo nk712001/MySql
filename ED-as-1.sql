3041f8fea00d6ffb7db0cab1a6adb54ece4d576a
Spr_zero <81459719+nk712001@users.noreply.github.com>
2022-03-01 14:07:19 +0530

    Add files via upload

---
+++ ED-as-1.sql
@@ -0,0 +1,8 @@
+show databases;
+USE Sprdb;
+SHOW tables;
+SELECT COUNT(Sname) FROM Salespeople WHERE Sname= 'A';
+SELECT * FROM orders where Amt > 2000;
+SELECT COUNT(Sname) FROM salespeople where City='Newyork';
+SELECT Snum FROM salespeople where City='London' and City='Paris';
+SELECT Onum FROM orders GROUP BY Odate ,Sname;
